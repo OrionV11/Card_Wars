757698376fd5a442cb8acc3e2e638a4c74051379
Final <Orionvia11@gmail.com>
2025-10-22 13:15:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
 import json
 import random
 import copy
+from creatures import Creature
+from card import Card
+import pygame
 
 try:
-    with open('Card_Wars/data.json', 'r', encoding='utf') as f:
+    with open('./data.json', 'r', encoding='utf') as f:
         cards_data = json.load(f)
 
 except Exception as e:
     print(f"Error loading data: {e}")
-    with open('Card_Wars/data.json', 'r', encoding='utf-8-sig') as f:
+    with open('./data.json', 'r', encoding='utf-8-sig') as f:
         cards_data = json.load(f)
 
 
 class Map():
     pass
 
-class Card():
-    def __init__(self, name, landscape, ability, cost):
-        self.name = name 
-        self.landscape = landscape
-        self.ability = ability
-        self.cost = cost
-    
-
-
-class Creature(Card):
-    def __init__(self, name, landscape, ability, cost, attack, defense):
-        super().__init__(name, landscape, ability, cost)     
-        self.attack = attack
-        self.defense = defense
-    
-    def facedown():
-        pass
-
-    def amaizing_avalanche():
-        #Each creature has +1 ATK this turn for each facedown landscape in play 
-        pass
 
 class Hero(Card):
     def __init__(self, name, ability):
@@ -43,29 +25,31 @@ class Hero(Card):
         self.name = name
         self.ability = ability
 
-
 class Spell(Card):
     def __init__(self, name, landscape, ability, cost):
         super().__init__(name, landscape, ability, cost)
-    
+
 class Building(Card):
     def __init__(self, name, landscape, ability, cost):
         super().__init__(name, landscape, ability, cost)
 
 class Landscape():
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pull_landscape():
+        for i in range(4):
+
+            pass
 
 creatures = cards_data.get('Creature', [])
 spells = cards_data.get('Spell', [])
 heroes = cards_data.get('Hero', [])
 buildings = cards_data.get('Building', [])
-
-
+landscape = cards_data.get('Landscape', [])
 
 class Menu():
     pass
-
-
 
 creature_objects = []
 for creature_data in creatures:
@@ -89,7 +73,7 @@ for creature_data in creatures:
             cost=spell_data['cost']
             )
         spells_objects.append(spell)
-    
+
     heroes_objects = []
     for hero_data in heroes:
         hero = Hero(
@@ -107,37 +91,65 @@ for creature_data in creatures:
             cost=building_data['cost']
             )
         buildings_objects.append(building)
-    
+    landscape_objects = []
+    for landscape_data in landscape:
+        landscape_card = Landscape(
+            name=landscape_data['name'],
+            )
+        landscape_objects.append(landscape_card)
+
 card_deck = {
     'Creatures' : creature_objects, 
     'Spells': spells_objects,
     'Heros': heroes_objects,
-    'Buildings': buildings_objects
+    'Buildings': buildings_objects,
+    }
+
+landscape_deck = {
+    'Landscapes': landscape_objects
     }
 
 playing_deck = copy.deepcopy(card_deck)
+landscape_playing_deck = copy.deepcopy(landscape_deck)
 drawn_cards = []
 
 def draw_random_card(deck):
     non_empty_types = [key for key, value in deck.items() if value]
-    
+
     if not non_empty_types:
         return None, None  # No cards left to draw
-    
+
     random_card_type = random.choice(non_empty_types)
     chosen_card_list = deck[random_card_type]
     random_card = random.choice(chosen_card_list)
     chosen_card_list.remove(random_card)
-    
+
     return random_card_type, random_card
 
-    
+
 class Player():
     def __init__(self, name, deck=playing_deck):
         self.name = name
         self.action = 2
         self.health = 25
-        self.hand = []    
+        self.hand = []
+        self.get_landscape_hand = []
+
+    def out_of_action(self):
+        if self.action <= 0:
+            print(f"{self.name} is out of actions for this turn.")
+            return True
+        return False
+
+    def reset_actions(self):
+        self.action = 2
+        print(f"{self.name}'s actions have been reset to {self.action} for the new turn.")
+
+    def lose_health(self, amount):
+        self.health -= amount
+        print(f"{self.name} loses {amount} health. Remaining health: {self.health}")
+        if self.health <= 0:
+            print(f"{self.name} has been defeated!")
 
     def starting_cards(self, deck, num_cards=5):
         print(f"\n{self.name} Drawing {num_cards} starting cards...")
@@ -148,10 +160,10 @@ class Player():
             else:
                 print("No more cards to draw.")
                 break
-    
+
     def cards_in_hand(self):
         print(f"\n{self.name}'s cards in hand:")
-        
+
         if not self.hand:
             print("No cards in hand.")
             return
@@ -160,31 +172,52 @@ class Player():
             temp_join = ' '.join(temp)
             card.name = temp_join
             print(f" - {card.name} ({type(card).__name__})")
-    
+
+
+    def play_card(self, card_index):
+        if 0 <= card_index < len(self.hand):
+            card = self.hand[card_index]
+            if self.action > 0:
+                self.action -= 1
+                played_card = self.hand.pop(card_index)
+                temp = played_card.name.split(' ')[3:]
+                temp_join = ' '.join(temp)
+                played_card.name = temp_join
+                print(f"{self.name} played {played_card.name}. Remaining actions: {self.action}")
+                return played_card
+            else:
+                print(f"{self.name} has no actions left to play a card.")
+        else:
+            print("Invalid card index.")
+        return None
+
     def pull_card(self, deck):
         card_type, card = draw_random_card(deck)
         temp = card.name.split(' ')[3:]
         temp_join = ' '.join(temp)
         card.name = temp_join
         if card:
+            self.action -= 1
             self.hand.append(card)
             print(f"{self.name} pulled a card: {card.name} ({card_type})")
         else:
             print("No more cards to draw.")
 
+    def get_landscape(self, deck, num_card):
+        print(f"\n{self.name} Drawing {num_card} landscape cards...")
+        for _ in range(num_card):
+            landscape_list = deck['Landscapes']
+            card = random.choice(landscape_list)
+            landscape_list.remove(card)
+            temp = card.name.split(' ')[3:]
+            temp_join = ' '.join(temp)
+            card.name = temp_join
+            if card:
+                self.get_landscape_hand.append(card)
+                print(f"{self.name} pulled a landscape card: {card.name})")
+            else:
+                print("No more landscape cards to draw.")
 
-    #2 Action (play Creature or buildings) Turns or pull
-
-    #creatues
-
-    #Buildings
-
-    #spells
-
-
-#40 cards
-
-#health 25
 
 class Game:
     def __init__(self):
@@ -204,7 +237,7 @@ class Game:
 class Menu:
     def __init__(self, options):
         self.options = options
-    
+
     def handle_input(self):
         # Handle key presses to navigate and select options
         selected_option = self.get_user_input()
@@ -214,28 +247,30 @@ class Menu:
             return "start_game"
         elif selected_option == "Quit":
             return "quit"
-    
+
     def draw(self):
         # Logic for drawing text and buttons on the screen
         pass
 
 def main():
-    
+
     Finn = Player("Finn")
     Jake = Player("Jake")
 
     Finn.starting_cards(playing_deck, 5)
     Jake.starting_cards(playing_deck, 5)
- 
-    print(Finn.cards_in_hand())
-    print(Finn.pull_card(playing_deck))
+
+    #print(Finn.cards_in_hand())
+    #print(Finn.pull_card(playing_deck))
+    #print(Finn.get_landscape(landscape_playing_deck, 4))
+
+
 
 
 '''
     print("Remaining cards in playing deck:")
     for card_type, card_list in playing_deck.items():
         print(f"Card Type: {card_type}, Card List: {card_list}")
-
     print("Original Deck")
     for card_type, card_list in card_deck.items():
         print(f"Card Type: {card_type}, Card List: {card_list}")
